392681e40266c153fd8c62f3ed8f452e56236946
leuhvraiminoryo <50586611+leuhvraiminoryo@users.noreply.github.com>
2022-03-18 19:59:14 +0100

    some progress towards #3

--- scripts/UI.py
+++ scripts/UI.py
@@ -1,5 +1,5 @@
 import threading, PySimpleGUI as sg
-from scripts.download import load as dl
+import scripts.download as dl
 
 sg.theme('DarkAmber')   # Add a touch of color
 # All the stuff inside your window.
@@ -16,7 +16,7 @@ def run():
         if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
             break
         if event == 'Ok':
-            load_thread = threading.Thread(target=dl,args=(values[0],))
+            load_thread = threading.Thread(target=dl.load,args=(values[0],))
             load_thread.start()
 
     window.close()
--- scripts/download.py
+++ scripts/download.py
@@ -1,8 +1,44 @@
 from __future__ import unicode_literals
-import threading
 import youtube_dl
 
-ydl_opts = {}
+status = {
+    'downloading': False,
+    'dl_of_name': None,
+    'percentage': None
+}
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    if '_percent_str' in d.keys():
+        status['dl_of_name'] = d['filename'][:-17]
+        status['percentage'] = d['_percent_str']
+    
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+        status['downloading'] = False
+
+
+ydl_opts = {
+    'format': 'best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+}
+
 def load(url):
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
